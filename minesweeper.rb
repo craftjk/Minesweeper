ab0b13d3c6da0a675459778c20dd1bc6849f719d
App Academy Student <student@appacademy.io>
2013-09-23 16:55:09 -0700

    First draft finished, still debugging

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,11 +1,14 @@
+# require 'debugger'
+
 class Tile
-  attr_reader :bomb, explored:, flagged:
-  def initialize(position, bomb, tiles)
+  attr_accessor :flagged
+  attr_reader :bomb, :explored, :revealed, :position
+  def initialize(position, bomb, board)
     @position = position
     @bomb = bomb
     @flagged = false
     @revealed = false
-    @tiles = tiles
+    @board = board
     @explored = false
   end
 
@@ -18,14 +21,16 @@ class Tile
     end
     @revealed = true
     @explored = true
-
+    p @bomb
+    p @position
     if @bomb
+      puts "Bomb 'triggered'"
       return false
     end
 
     if adjacent_bombs == 0
       neighbors.each do |adj_tile|
-        tile = @tiles[adj_tile[0]][adj_tile[1]]
+        tile = @board.tiles[adj_tile[0]][adj_tile[1]]
         tile.explore unless (tile.explored || tile.flagged)
       end
     end
@@ -35,13 +40,16 @@ class Tile
   def display
     if @revealed
       if adjacent_bombs > 0
-        return adjacent_bombs
+        return " #{adjacent_bombs.to_s} "
       else
-        return "_"
+        return " _ "
       end
+    elsif @bomb
+      return " B "
     else
-      return "*" unless @flagged
-      return "F"
+      return " * " unless @flagged
+      @flagged = true
+      return " F "
     end
   end
 
@@ -57,7 +65,7 @@ class Tile
       end
     end
     neighbors.delete_if do |neighbor|
-      neighbor[0] > @tiles.length || neighbor[0] < 0 || neighbor[1] > @tiles.first.length || neighbor[1] < 0
+      neighbor[0] > @board.tiles.length - 1 || neighbor[0] < 0 || neighbor[1] > @board.tiles.first.length - 1 || neighbor[1] < 0
     end
     neighbors
   end
@@ -65,13 +73,14 @@ class Tile
   def adjacent_bombs
     bomb_count = 0
     neighbors.each do |neighbor|
-      bomb_count += 1 if @tiles[neighbor[0]][neighbor[1]].bomb
+      bomb_count += 1 if @board.tiles[neighbor[0]][neighbor[1]].bomb
     end
     bomb_count
   end
 end
 
 class Board
+  attr_accessor :tiles
   def initialize(size, num_mines)
     @size = size
     @num_mines = num_mines
@@ -81,12 +90,12 @@ class Board
   def generate_board
     mine_array = generate_bombs
     board = []
-    size[0].times do |row|
-      row = []
-      size[1].times do |col|
-        row << Tile.new([row, col], mine_array.pop, @size)
+    @size[0].times do |row|
+      row_arr = []
+      @size[1].times do |col|
+        row_arr << Tile.new([row, col], mine_array.pop, self)
       end
-      board << row
+      board << row_arr
     end
     board
   end
@@ -105,23 +114,30 @@ class Board
   end
 
   def run
-    generate_board
+    @board = generate_board
+    revealed_bomb = false
     until won? || revealed_bomb
+
+      display_board
       action = prompt_action
       tile_position = prompt_tile
       tile = @board[tile_position[0]][tile_position[1]]
       if action == "R"
-        revealed_bomb = !tile.explore
+        tile.explore
+        display_board
+        revealed_bomb = tile.bomb
+        p "Revealed bomb is #{revealed_bomb}"
         lose if revealed_bomb
       elsif action == "F"
-        tile.flag
+        tile.flagged = true
+        p tile.position
+        p tile.flagged
       else
         puts "Please input R or F for action."
         next
       end
-
-      display_board
     end
+    display_board
   end
 
   def display_board
@@ -131,6 +147,7 @@ class Board
       end
       puts
     end
+    puts
   end
 
   def won?
@@ -145,7 +162,10 @@ class Board
   end
 
   def lose
+    puts
+    puts "You lose"
     puts "(x_x)"
+    puts
   end
 
   def save
@@ -156,7 +176,7 @@ class Board
 
   def prompt_action
     puts "[R]eveal or [F]lag?"
-    gets.chomp
+    gets.chomp.upcase
   end
 
   def prompt_tile
